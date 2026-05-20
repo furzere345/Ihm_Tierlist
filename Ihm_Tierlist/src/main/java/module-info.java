618a98f3a32f280eacc553a3b6b386d3ffab3d79
Amaury Bernard <i2503927@cri.univ-lemans.fr>
2026-05-20 16:02:35 +0200

    package view + modul info

--- Ihm_Tierlist/src/main/java/module-info.java
+++ Ihm_Tierlist/src/main/java/module-info.java
@@ -2,9 +2,6 @@ module org.example.ihm_tierlist {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens tierlist to javafx.fxml, javafx.graphics;
-
-    opens tierlist.controller to javafx.fxml;
-
+    opens tierlist.view to javafx.graphics, javafx.fxml;
     exports tierlist;
 }
